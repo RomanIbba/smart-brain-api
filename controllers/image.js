e59799428ff0e43c62f5294d2ba78658ebdf7554
RomanIbba <romanibba95@gmail.com>
2021-01-19 20:08:32 -0800

    Creating server repository

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+    apiKey: '7859e1c0b7864510b6da42f4173b6f8b'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+const images = {
+    handleApiCall,
+    handleImage
+}
+
+export default images;
---
+++ server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import bcrypt from 'bcrypt';
+import cors from 'cors';
+import knex from 'knex';
+
+import handleRegister from './controllers/register.js';
+import handleSignIn from './controllers/signin.js';
+import handleProfileGet from './controllers/profile.js';
+// import hanldeImage from './controllers/image.js';
+// import handleApiCall from './controllers/image.js';
+import images from './controllers/image.js';
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1', // this number represents localhost aka home
+        user: 'postgres',
+        password: 'Theman1231',
+        database: 'smart-brain'
+    }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => { res.send('success') })
+app.post('/signin', (req, res) => { handleSignIn(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { images.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { images.handleApiCall(req, res) })
+
+app.listen(3000, () => {
+    console.log('app is running');
+})
+
+/*
+/ --> res = this is working
+/signin --> POST = success/ fail
+/register --> POST = user
+/profile:userId --> GET = user
+/image --> PUT --> user
+
+*/
